68276b637d8a8df14720f7d595f7063be6cf8f70
Paulo Henrique <66080380+Paulo-henrique-otk@users.noreply.github.com>
2020-06-06 19:31:50 -0300

    Update excluirmens.php

--- excluirmens.php
+++ excluirmens.php
@@ -2,7 +2,7 @@
 
 require_once './Conexao.php';
 //pega o id da mensagem via URL 
-$idmens = mysqli_escape_string($con, filter_input(INPUT_GET, "idmens",FILTER_VALIDATE_INT,FILTER_SANITIZE_NUMBER_INT,FILTER_SANITIZE_SPECIAL_CHARS));
+$idmens = mysqli_escape_string($con, filter_input(INPUT_GET, "idmens",FILTER_SANITIZE_NUMBER_INT,FILTER_SANITIZE_SPECIAL_CHARS));
 //deleta do banco
 $delete  = mysqli_query($con, "delete from mensagem where idmens = '$idmens'");
 //redireciona para a pagina principal
